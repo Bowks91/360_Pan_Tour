f16f93d21717ae46335ca784476f2f6ce62a1fd8
Alex <alexander.bowkley@durham.ac.uk>
2023-02-20 14:31:48 +0000

    360 Pan Tour

---
+++ app-files/data.js
@@ -0,0 +1,77 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-5481b00-natural-landscape-360-aerial",
+      "name": "5481b00-natural-landscape-360-aerial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.282567433577812,
+        "pitch": -0.0706397406228092,
+        "fov": 1.4745706707546977
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.066216381173044,
+          "pitch": 0.5059257450924619,
+          "title": "This is a river",
+          "text": "Rivers are wet"
+        },
+        {
+          "yaw": 0.4205418483904708,
+          "pitch": 0.3494503823613826,
+          "title": "Houses",
+          "text": "Where people live"
+        },
+        {
+          "yaw": -2.0811622578107727,
+          "pitch": 0.6604531965332043,
+          "title": "Baseball field",
+          "text": "A field for playing baseball"
+        },
+        {
+          "yaw": -2.809173727817239,
+          "pitch": 0.580527351104994,
+          "title": "Football pitches",
+          "text": "GOOOOAAAALLLL"
+        },
+        {
+          "yaw": 1.2278822894092603,
+          "pitch": -0.03293127669608786,
+          "title": "Skyscrapers",
+          "text": "Probably for banking...or insurance"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
